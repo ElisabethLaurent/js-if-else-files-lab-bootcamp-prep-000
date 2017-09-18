04a3a76e0ae4ab5e210c7811462973b26a3dc851
Elisabeth L <elisabethsochacki@gmail.com>
2017-09-18 21:29:08 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -1,2 +1,14 @@
 console.log(name)
 var index = require("./index.js")
+
+if (name === "Susan") {
+    console.log("The name, Susan, is correct")
+} else {
+    console.log("Expected the name to be Susan, but received: "+index.name)
+)}
+
+if (height === "74") {
+  console.log("Height is correct at 74.")
+} else {
+  console.log("Expected height to be 74, but received: "+index.height)
+)}
