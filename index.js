dd70b9bdf373b2ddcfd8eb1e6cc24e07d2d178ff
Elisabeth L <elisabethsochacki@gmail.com>
2017-09-18 21:13:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ module.exports = { name, height
 if (name === "Susan") {
     console.log("The name, Susan, is correct")
 } else {
-    console.log("Expected the name to be Susan, but received "+index.name)
+    console.log("Expected the name to be Susan, but received "+.name)
 }
 if (height === "74") {
   console.log("Height is correct at 74.")
 } else {
-  console.log("Expected height to be 74, but received "+index.height)
+  console.log("Expected height to be 74, but received "+.height)
 }
