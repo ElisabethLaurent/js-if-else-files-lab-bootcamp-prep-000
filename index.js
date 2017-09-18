3ddc8db5846dc168c9327f0616a2d3a30721974c
Elisabeth L <elisabethsochacki@gmail.com>
2017-09-18 21:29:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,15 +5,3 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-
-if (name === "Susan") {
-    console.log("The name, Susan, is correct")
-} else {
-    console.log("Expected the name to be Susan, but received ("var.name")"
-)}
-
-if (height === "74") {
-  console.log("Height is correct at 74.")
-} else {
-  console.log("Expected height to be 74, but received ("var.height")"
-)}
